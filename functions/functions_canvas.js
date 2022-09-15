7e623955d53b7606b5d31c83682367fb7e437e47
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2022-09-16 00:46:53 +0200

    new version

--- functions/functions_canvas.js
+++ functions/functions_canvas.js
@@ -20,37 +20,37 @@ function canvas_create_position_and_draw(ctx,number_canvas) {
       if (eventful_list[number_canvas][i] > uneventful_list[number_canvas][i] && annoying_list[number_canvas][i] >= pleasant_list[number_canvas][i]) {
         // Primer cuadrante.
         // Point x menor de 198 y point y menor de 151
-        point_x = (Math.sin(angulo)*radius)+center_x;
-        point_y = -(Math.cos(angulo)*radius)+center_y;
+        point_x = (Math.sin(angulo)*point_x)+center_x;
+        point_y = -(Math.cos(angulo)*point_y)+center_y;
       }
     
       if (eventful_list[number_canvas][i] > uneventful_list[number_canvas][i] && pleasant_list[number_canvas][i] >= annoying_list[number_canvas][i]) {
           // Segundo Cuadrante
           // Point x mayor de 198 y point y menor de 151
-          point_x = (Math.sin(angulo)*radius)+center_x;
-          point_y = -(Math.cos(angulo)*radius)+center_y;
+          point_x = (Math.sin(angulo)*point_x)+center_x;
+          point_y = -(Math.cos(angulo)*point_y)+center_y;
       }
       
       if (uneventful_list[number_canvas][i] > eventful_list[number_canvas][i] && pleasant_list[number_canvas][i] >= annoying_list[number_canvas][i]) {
           // Tercer Cuadrante
           // Point x menor de 198 y point y mayor de 151
-          point_x = -(Math.sin(angulo)*radius)+center_x;
-          point_y = (Math.cos(angulo)*radius)+center_y;
+          point_x = -(Math.sin(angulo)*point_x)+center_x;
+          point_y = (Math.cos(angulo)*point_y)+center_y;
       }
       
       if (uneventful_list[number_canvas][i] > eventful_list[number_canvas][i] && annoying_list[number_canvas][i] >= pleasant_list[number_canvas][i]) {
           // Cuarto Cuadrante
           // Point x mayor de 198 y point y mayor de 151
-          point_x = -(Math.sin(angulo)*radius)+center_x;
-          point_y = (Math.cos(angulo)*radius)+center_y;
+          point_x = -(Math.sin(angulo)*point_x)+center_x;
+          point_y = (Math.cos(angulo)*point_y)+center_y;
       }
 
       if (eventful_list[number_canvas][i] == uneventful_list[number_canvas][i]) {
         point_y = center_y;
         if (pleasant_list[number_canvas][i]> annoying_list[number_canvas][i]) {
-            point_x = (Math.sin(angulo)*radius)+center_x;
+            point_x = (Math.sin(angulo)*point_x)+center_x;
         }else if (annoying_list[number_canvas][i] > pleasant_list[number_canvas][i]) {
-            point_x = -(Math.sin(angulo)*radius)+center_x;
+            point_x = -(Math.sin(angulo)*point_x)+center_x;
         }else{
             point_x = center_x;
         }
