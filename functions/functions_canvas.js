cc02d899ce3c9cefb30a7378d7a320691817dedd
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2022-10-01 01:03:51 +0200

    Fix Bug Canvas

--- functions/functions_canvas.js
+++ functions/functions_canvas.js
@@ -1,6 +1,7 @@
 const center_x = 198;
 const center_y = 151;
 const radius = 116;
+//const max_value = 1+Math.sqrt(2);
 const max_value = 1+Math.sqrt(2);
 
 function canvas_create_position_and_draw(ctx,number_canvas) {
@@ -9,60 +10,34 @@ function canvas_create_position_and_draw(ctx,number_canvas) {
     for (let i = 0; i < pleasant_list[0].length; i++) {
       x = (pleasant_list[number_canvas][i]-annoying_list[number_canvas][i])+Math.cos(Math.PI/4)*(calm_list[number_canvas][i]-chaotic_list[number_canvas][i])+Math.cos(Math.PI/4)*(vibrant_list[number_canvas][i]-monotonous_list[number_canvas][i]);
       x = x / max_value;
-      point_x = radius*x;
-
+     
       y = (eventful_list[number_canvas][i]-uneventful_list[number_canvas][i])+Math.cos(Math.PI/4)*(chaotic_list[number_canvas][i]-calm_list[number_canvas][i])+Math.cos(Math.PI/4)*(vibrant_list[number_canvas][i]-monotonous_list[number_canvas][i]);
       y = y / max_value;
-      point_y = radius*y;
       
-      let angulo = Math.atan(point_x/point_y);
+      console.log(x,y)
 
-      if (eventful_list[number_canvas][i] > uneventful_list[number_canvas][i] && annoying_list[number_canvas][i] >= pleasant_list[number_canvas][i]) {
-        // Primer cuadrante.
-        // Point x menor de 198 y point y menor de 151
-        point_x = (Math.sin(angulo)*point_x)+center_x;
-        point_y = -(Math.cos(angulo)*point_y)+center_y;
-      }
-    
-      if (eventful_list[number_canvas][i] > uneventful_list[number_canvas][i] && pleasant_list[number_canvas][i] >= annoying_list[number_canvas][i]) {
-          // Segundo Cuadrante
-          // Point x mayor de 198 y point y menor de 151
-          point_x = (Math.sin(angulo)*point_x)+center_x;
-          point_y = -(Math.cos(angulo)*point_y)+center_y;
-      }
-      
-      if (uneventful_list[number_canvas][i] > eventful_list[number_canvas][i] && pleasant_list[number_canvas][i] >= annoying_list[number_canvas][i]) {
-          // Tercer Cuadrante
-          // Point x menor de 198 y point y mayor de 151
-          point_x = -(Math.sin(angulo)*point_x)+center_x;
-          point_y = (Math.cos(angulo)*point_y)+center_y;
-      }
-      
-      if (uneventful_list[number_canvas][i] > eventful_list[number_canvas][i] && annoying_list[number_canvas][i] >= pleasant_list[number_canvas][i]) {
-          // Cuarto Cuadrante
-          // Point x mayor de 198 y point y mayor de 151
-          point_x = -(Math.sin(angulo)*point_x)+center_x;
-          point_y = (Math.cos(angulo)*point_y)+center_y;
+      // Calculamos el ángulo.
+        var angulo = Math.atan(y/x);
+      // Si el punto está en los cuadrantes tercero o cuarto
+      if (x < 0) {
+        // Calculamos el ángulo del tercer o cuarto cuadrante.
+        angulo += (Math.PI);
       }
 
-      if (eventful_list[number_canvas][i] == uneventful_list[number_canvas][i]) {
-        point_y = center_y;
-        if (pleasant_list[number_canvas][i]> annoying_list[number_canvas][i]) {
-            point_x = (Math.sin(angulo)*point_x)+center_x;
-        }else if (annoying_list[number_canvas][i] > pleasant_list[number_canvas][i]) {
-            point_x = -(Math.sin(angulo)*point_x)+center_x;
-        }else{
-            point_x = center_x;
-        }
-        console.log("Eventful or not",eventful_list[number_canvas][i] == uneventful_list[number_canvas][i])
-    }
+      // Normalizamos la x y la y.
+      x = Math.cos(angulo);
+      y = Math.sin(angulo);
+      //Obtenemos el módulo y lo extrapolamos al tamaño de la circunferencia.
+      var z = Math.sqrt((x*x)+(y*y))*radius;
+      point_x = (z * Math.cos(angulo)) + center_x;
+      point_y = -(z * Math.sin(angulo)) + center_y;
 
-      console.log("Coordenada X",point_x,"Coordenada Y",point_y)
+      //console.log("Coordenada X",point_x,"Coordenada Y",point_y)
 
       coordenada_x[i] = point_x;
       coordenada_y[i] = point_y;
       
-    } 
+    }
     canvas_draw_hex(ctx);
     canvas_draw_points(ctx,coordenada_x,coordenada_y);
 }
