09d4592a82ea349474c403d9f71ffda6df47322c
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2023-06-30 17:54:06 +0200

    Versión Final Versión 2.0.0

--- functions/Canvas/Canvas.js
+++ functions/Canvas/Canvas.js
@@ -1,18 +1,40 @@
-const ChronologyChart = require('./DrawChronologyGraph')
-const ParticipantChart = require('./DrawParticipantGraph')
+//const ChronologyChart = require('./DrawChronologyGraph')
+//const ParticipantChart = require('./DrawParticipantGraph')
 const ScenaryRadarGraph = require('./DrawScenaryGraph')
 const ScenaryInformation = require('./DrawInformationScenary')
 const ScenaryScatterGraph = require('./DrawScatterGraph')
 
 
+function resetAllCanvas() {
+    //Reseteamos la cronología.
+    document.getElementById("chronologyCanvas").innerHTML = '<canvas id="chronologyChart"></canvas>';
+    //Reseteamos el canvas de los graficos del participante
+    document.getElementById("participantCanvas").innerHTML = '<canvas id="participantChart"></canvas>';
+    // Primer Gráfico izquierdo y derecho - reset 
+    document.getElementById("graphLeft1").innerHTML = '<canvas id="renderRadarChart1"></canvas>';
+    //
+    document.getElementById("graphRight1").innerHTML = '<canvas id="renderPieChart1"></canvas>';
+    // Segundo Gráfico izquierdo y derech - reset
+    document.getElementById("graphLeft2").innerHTML = '<canvas id="renderRadarChart2"></canvas>';
+    //
+    document.getElementById("graphRight2").innerHTML = '<canvas id="renderPieChart2"></canvas>';
+    // Tercero Gráfico izquierdo y derech - reset
+    document.getElementById("graphLeft3").innerHTML = '<canvas id="renderRadarChart3"></canvas>';
+    //
+    document.getElementById("graphRight3").innerHTML = '<canvas id="renderPieChart3"></canvas>';
+}
+
+
 function Draw() {
-    console.info("Dentro Canvas")
+
     numberTotalPlaces = quiz_info[0][0]["Places_Number"];
     numberTotalRecordings = quiz_info[0][0]["Recordings_Number"];
     numberTotalReplies = numberTotalPlaces*numberTotalRecordings;
-
-    ChronologyChart.drawChronologyGraph();
-    ParticipantChart.drawParticipantGraph(2);
+    resetAllCanvas();
+    //ChronologyChart.drawChronologyGraph();
+    drawChronologyGraph();
+    //ParticipantChart.drawParticipantGraph(2);
+    drawParticipantGraph(2);
 
     for (let numberPlace = 1; numberPlace <= numberTotalPlaces; numberPlace++) {
         document.getElementById("scenaryGraph"+(numberPlace)).style.display = "block";
